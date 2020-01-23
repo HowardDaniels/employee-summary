92952d853d8992f19ef8eb4e0d42c676bec40d94
Howard Daniels <hdaniels13tat@gmail.com>
2020-01-23 13:33:04 -0500

    installed NPM Inquirer and began editing the parent class test file

---
+++ test/Employee.test.js
@@ -0,0 +1,24 @@
+class Employee {
+    constructor(name, id, title){
+      this.name= name;
+      this.id = id;
+      this.title = title;
+    }
+   /* getName() {
+    //  return 'I have a ' + this.carname;
+    }
+    getEmail() {
+      //  return 'I have a ' + this.carname;
+    } */
+    getRole() {
+        //  return 'I have a ' + this.carname;
+        console.log(this.name);
+      }
+  }
+
+  /*getName()
+  * getId()
+  * getEmail()
+  */ getRole() // Returns 'Employee'
+
+  module.exports = Employee;
