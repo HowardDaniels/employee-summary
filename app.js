344b7a84749d2148229c73e34bc2aa5b1cdd425c
Howard Daniels <hdaniels13tat@gmail.com>
2020-01-28 14:38:41 -0500

    put code for recursive prompt in the comments

--- app.js
+++ app.js
@@ -110,3 +110,45 @@ else if (answers.teamMembers === yesNo[1]){
 }
 });
 
+/*
+inquirer.registerPrompt('recursive', require('inquirer-recursive'));
+inquirer.prompt([{
+    type: 'recursive',
+    message: 'Add a new user ?',
+    name: 'users',
+    prompts: [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is user\'s name?',
+            validate: function (value) {
+                if ((/.+/).test(value)) { return true; }
+                return 'name is required';
+            }
+        }, {
+            type: 'input',
+            name: 'age',
+            message: 'How old is he?',
+            validate: function (value) {
+                var digitsOnly = /\d+/;
+                if (digitsOnly.test(value)) { return true; }
+                return 'Invalid age! Must be a number genius!';
+            }
+        }
+    ]
+}]).then(function(answers) {
+    console.log(answers.users);
+    /*
+    OUTPUT :
+    [
+        {
+            name: 'Brendan Eich',
+            age: '42',
+        }, {
+            name: 'Jordan Walke',
+            age: '13',
+        },
+        ...
+    ]
+    
+}); */
