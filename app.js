09355e647c8dfbe09063a61ec6a8c3255843597f
Howard Daniels <hdaniels13tat@gmail.com>
2020-01-31 16:56:58 -0500

    entered in all 14 HTML code sections to be conditionally output to index.html; deleted unnecessary comments at the end of the code

--- app.js
+++ app.js
@@ -160,7 +160,7 @@ var currentSchoolNotLast =
 </div>
 </div>`
 
-fs.appendFile("output/index.html", InternRow, function(err) {
+fs.appendFile("output/index.html", currentSchoolNotLast, function(err) {
   if (err) {
     throw err;
   }
@@ -255,12 +255,48 @@ function askTeamMemberQuestions2(){
           console.log("000");
           /* HTML Code #11: GitHub table row + end of document for last team member entered who is also an engineer. */
 
+          var GitHubPlusEnd2 = 
+
+          
+          `<tr><td>GitHub account: <a href="https://www.github.com/${gitAskAgain.GitHub}">${gitAskAgain.GitHub}</a></td></tr>
+          </table>
+          </div>
+          </div>
+          </div>
+      </body>
+  </html>`;
+
+  fs.appendFile("output/index.html", GitHubPlusEnd2, function(err) {
+    if (err) {
+      throw err;
+    }
+  });
         }
         })
       }
     else if (teamMemberQuestions.role == "Intern") {
       console.log(teamMemberQuestions.role);
             /* HTML Code #12: Intern info up to but not including the current school */
+
+            var InternRow2 =
+            `<br>
+                    <div class="row">
+                        <div class="col-md-3">
+                     <div class="row" id="top">
+                         <br>
+                         <p id="name">${teamMemberQuestions.teamMemberName}</p>
+                         <p id="role">Intern</p>
+                         <br>
+                     </div>
+                    <div class="row" id="bottom">
+                        <table id="infotable">
+                            <tr><td>ID: ${teamMemberQuestions.teamMemberID}</td></tr>`
+            
+                    fs.appendFile("output/index.html", InternRow2, function(err) {
+                      if (err) {
+                        throw err;
+                      }
+                    });
 
       inquirer
       .prompt(schoolAskAgain)
@@ -269,10 +305,41 @@ function askTeamMemberQuestions2(){
           console.log("//");
             /* HTML Code #13: Current school info for interns other than the last team member */
 
+            var currentSchoolNotLast2 = 
+
+            `<tr><td>Current School: ${schoolAskAgain.currentSchool}</td></tr>
+            </table>
+            </div>
+            </div>
+            </div>`
+            
+            fs.appendFile("output/index.html", currentSchoolNotLast2, function(err) {
+              if (err) {
+                throw err;
+              }
+            });
+
+
           askTeamMemberQuestions();
         }
         else if (schoolAskAgain.addAnotherTeamMember == "No"){
             /* HTML Code #14: Current school info for interns + end of code for last team member */
+
+            var currentSchoolLast2 =
+
+            `<tr><td>Current School: George Washington University</td></tr>
+            </table>
+            </div>
+            </div>
+            </div>
+            </body>
+            </html>`
+            
+            fs.appendFile("output/index.html", currentSchoolLast2, function(err) {
+              if (err) {
+                throw err;
+              }
+            });
 
           console.log("000");
         }
@@ -282,26 +349,6 @@ function askTeamMemberQuestions2(){
 
 }
 
-
-/*
-function askTeamMemberQuestions(){
-  inquirer
-  .prompt(teamMemberQuestions)
-  .then(teamMemberQuestions => {
-    if (teamMemberQuestions.role = roleChoices[0]){
-      inquirer
-      .prompt(GitHubQuestion, askAgain)
-      .then(askTeamMemberQuestions())
-    }
-    else { 
-    inquirer
-    .prompt(currentSchoolQuestion, askAgain)
-    .then(askTeamMemberQuestions())
-    }
-  })
-}
-
-*/
 /* This prompts the user about the manager (these are required fields).
 The user is then asked how many engineers he or she would like to add to the profile. */
 
@@ -424,193 +471,4 @@ else{
   });
   console.log("Profile of manager is ready to view.");
 }
-
-
-/*
-
-  if (ManagerQuestions.teamMembers > 0){
- //   teamMemberQuestions = [];
-    console.log("STARTS HERE");
-    console.timeLog(JSON.stringify(ManagerQuestions.teamMembers));
-    console.log(ManagerQuestions);
-
-    .forEach(patron => console.log(patron.age));
-
-    /*
-    for (i = 1; i < (ManagerQuestions.teamMembers + 1); i++){
-        teamMemberQuestions.push(
-      {
-        type: "input",
-        message: "Engineer #" + i  + "'s name:",
-        name: "engineerName" + i,
-      },
-      {
-        type: "number",
-        message: "Engineer #" + i + "'s ID:",
-        name: "engineerID" + i,
-      },
-      {
-        type: "input",
-        message: "Engineer #" + i + "'s GitHub:",
-        name: "engineerGitHub" + i,
-      });
-    }
-*/
-/* This question asks how many interns the user would like to provide, and is only asked once. */
-/*
-    engineerQuestions.push(
-      {
-        type: "number",
-        message: "How many interns would you like to add?",
-        name: "interns"
-      });
-      */
-/*
-      const engineerNum = ((engineerQuestions.length - 1) / 3);
-*/
-/*
-inquirer
-.prompt(engineerQuestions)
-.then(engineerQuestions => {
-  console.log("MY PRINT")
-  console.log(engineerQuestions);
-/*
-
-var engineerNameNumbers = [];
-var engineerIDNumbers = [];
-var engineerGitHubNumbers = [];
-var engineerRows = [];
-  for (i = 1; i < (engineerNum + 1); i++){
-    engineerNameNumbers.push("answers.engineerName" + i)
-    engineerIDNumbers.push("answers.engineerID" + i)
-    engineerGitHubNumbers.push("answers.engineerGitHub" + i)
-  }
-
-  console.log(engineerNameNumbers);
-  console.log(engineerIDNumbers);
-  console.log(engineerGitHubNumbers);
-
-  for (i = 0; i < (engineerNum); i++){
-    engineerRows.push(`<div class="row">
-    <div class="col-md-3">
-    <div class="row" id="top">
-     <br>
-     <p id="name">${engineerNameNumbers[i]}</p>
-     ${answers.engineerGithub}
-     <p id="role">Engineer</p>
-     <br>
-    </div>
-    <div class="row" id="bottom">
-    <table id="infotable">
-        <tr><td>ID: ${engineerIDNumbers[i]}</td></tr>
-        <tr><td>GitHub: ${engineerGitHubNumbers[i]}</td></tr>
-    </table>
-    </div>
-    </div>
-    </div>`);
-  }
-
-const formattedEngineerRows = engineerRows.join("");
-
-  console.log(engineerRows);
-
-  const engineerAndManagerProfile = `<html>
-  <head>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="../templates/assets/manager.css">
-  </head>
-  <body>
-  ${managerRow}
-        ${formattedEngineerRows}
-  </body>
-  </html>`;
-*/
-
-/* Prompts the user about the interns. */
-/*
-    if (engineerQuestions.interns > 0){
-      internQuestions = [];
-      for (i = 1; i < (engineerQuestions.interns + 1); i++){
-        internQuestions.push({
-         type: "input",
-         message: "Intern #" + i  + "'s name:",
-         name: "internName" + i,
-       },
-       {
-         type: "number",
-         message: "Intern #" + i + "'s ID:",
-         name: "internID" + i,
-       },
-       {
-         type: "input",
-         message: "Intern #" + i + "'s current school:",
-         name: "internCurrentSchool" + i,
-       }
-       );
-       }
-       console.log(internQuestions);
-       inquirer
-       .prompt(internQuestions);
-     
-   //      console.log("Profile of manager, engineers, and interns is ready.")
-       
-    }
-    else {
-      fs.writeFile("output/index.html", engineerAndManagerProfile, function(err) {
-        if (err) {
-          throw err;
-        }
-      });
-      console.log("Profile of manager and engineers ready to view.");
-    }
-
-  });
-}
-
-/*
-  else {
-    inquirer
-    .prompt([
-      {
-      type: "number",
-      message: "How many interns would you like to add?",
-      name: "interns2"
-      }
-])
-  .then(answers => {
-    if (answers.interns2 > 0){
-      arr3 = [];
-      for (i = 1; i < (answers.interns2 + 1); i++){
-        arr3.push({
-         type: "input",
-         message: "Intern #" + i  + "'s name:",
-         name: "internName" + i,
-       },
-       {
-         type: "number",
-         message: "Intern #" + i + "'s ID:",
-         name: "internID" + i,
-       },
-       {
-         type: "input",
-         message: "Intern #" + i + "'s current school:",
-         name: "internCurrentSchool" + i,
-       }
-       );
-       }
-       console.log(arr3);
-       inquirer
-       .prompt(arr3)
-    }
-
-    else {
-    
-    fs.writeFile("output/index.html", managerProfile, function(err) {
-      if (err) {
-        throw err; 
-      }
-    });
-    }
-  });
-}*/
 });
